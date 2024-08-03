4450687c53d0aea0b61af0b4109dc36c3099d8e5
abhinavarya <abhinavarya330@gmail.com>
2024-08-03 20:55:32 +0530

    palindrome restructure

--- src/interview_asked/Palindrome.java
+++ src/interview_asked/Palindrome.java
@@ -1,7 +1,12 @@
 package interview_asked;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Palindrome {
     public static void main(String[] args) {
+//        String s = "malayalam";
+//        System.out.println(isPalindrome(s));
     }
 
     private static boolean isPalindrome(String str) {
@@ -13,5 +18,21 @@ public class Palindrome {
             s++; e--;
         }
         return true;
+    }
+
+    ////////////////////////////////////////////////////////////////////
+    public static int binary(int[] arr, int tar){
+        int si=0,ei=arr.length-1;
+        while(si<=ei){
+            int mid = (si+ei)/2;
+            if(arr[mid]==tar){
+                return mid;
+            } else if(arr[mid]<tar){
+                si=mid+1;
+            } else{
+                ei=mid-1;
+            }
+        }
+        return -1;
     }
 }
